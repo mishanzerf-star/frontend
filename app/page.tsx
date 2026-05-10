59d4bdc41f9289f0d04d1bb90d8454c8704d5ad2
mishanzer <mishanzerf@gmail.com>
2026-05-11 01:57:37 +0300

    fixed page

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,7 @@
-"use client";
-
-import { useState } from "react";
-
 export default function Home() {
-  const [connected, setConnected] = useState(false);
-  const [page, setPage] = useState("home");
-
   return (
-    <div>
+    <main>
       <h1>Hello</h1>
-    </div>
+    </main>
   );
 }
